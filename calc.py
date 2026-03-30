0aeb29224c682ccccae57e1ed30e06286ae3001a
18prabin <np03cs4s250078@heraldcollege.edu.np>
2026-03-30 12:09:29 +0545

    completed subtract

--- calc.py
+++ calc.py
@@ -1,8 +1,9 @@
+
 try:
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
 
-    result = num1 * num2
+    result = num1 - num2
     print("Answer:", result)
 
 except ValueError:
